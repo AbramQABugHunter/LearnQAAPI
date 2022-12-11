b53eb349a2da3f361da3eef900042d4dfbb86234
AbramQABugHunter <AbramQa@ya.ry>
2022-12-11 22:25:04 +0300

    Добавил assertions.py

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -1,6 +1,8 @@
 import pytest
 import requests
 from Libary.base_case import BaseCase
+from Libary.assertions import Assertions
+
 
 class TestUserAuth(BaseCase):
     exclude_params = [
@@ -17,9 +19,8 @@ class TestUserAuth(BaseCase):
         response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
 
         self.auth_sid = self.get_cookie(response1, "auth_sid")
-        self.token = self.get_header(response1, "s-csrf-token")
+        self.token = self.get_header(response1, "x-csrf-token")
         self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
-
 
     def test_auth_user(self):
         response2 = requests.get(
@@ -28,9 +29,12 @@ class TestUserAuth(BaseCase):
             cookies={"auth_sid": self.auth_sid}
         )
 
-        assert "user_id" in response2.json(), "there is no user id in the second response"
-        user_id_from_check_method = response2.json()['user_id']
-        assert self.user_id_from_auth_method == user_id_from_check_method, "User id from auth method is no equal to user id from check method"
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            self.user_id_from_auth_method,
+            "User id from auth method is not equal to user id from check method"
+        )
 
     @pytest.mark.parametrize("condition", exclude_params)
     def test_negative_auth_check(self, condition):
@@ -44,8 +48,10 @@ class TestUserAuth(BaseCase):
                 "https://playground.learnqa.ru/api/user/auth",
                 cookies={"auth_sid": self.auth_sid}
             )
-        assert "user_id" in response2.json(), "there is no user id in the second response"
 
-        user_id_from_check_method = response2.json()["user_id"]
-
-        assert user_id_from_check_method == 0, f"User is authorized with condition {condition}"
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            0,
+            f"User is authorized with {condition}"
+        )
