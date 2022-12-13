f4a9ef8acc726d5f3d4d722dfa70af3f04284412
AbramQaBugHunter <AbramQa@ya.ru>
2022-12-13 15:17:54 +0200

    add test_user_successfully

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,8 +1,28 @@
 import requests
 from Libary.base_case import BaseCase
 from Libary.assertions import Assertions
+from datetime import datetime
 
 class TestUserRegister(BaseCase):
+    def setup_method(self):
+        base_part = "learnqa"
+        domain = "example.com"
+        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+        self.email = f"{base_part}{random_part}@{domain}"
+
+    def test_user_successfully(self):
+        data = {
+            "password": "123",
+            "username": "learnqa",
+            "firstName": "learnqa",
+            "lastName": "learnqa",
+            "email": self.email
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        assert response.status_code == 200, f"Unexpected status code {response.status_code}"
+        print(response.content)
     def test_create_user_with_existing_email(self):
         email = "vinkotov@example.com"
         data = {
