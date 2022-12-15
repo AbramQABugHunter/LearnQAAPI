a782c427eac0fd7087a69215b5d7bb488161f85f
AbramQaBugHunter <AbramQa@ya.ru>
2022-12-15 16:49:36 +0200

    add test_edit_just_created_user

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -4,3 +4,14 @@ from Libary.assertions import Assertions
 
 class TestUserEdit(BaseCase):
     def test_edit_just_created_user(self):
+        register_data = self.prepare_registration_data()
+        response1 = requests.post("https://playground.learnqa.ru/api/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data["email"]
+        first_name = register_data["firstName"]
+        last_name = register_data["lastName"]
+        password = register_data["password"]
+
