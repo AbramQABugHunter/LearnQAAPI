4b10394e3c4302833938892e023922d99ab731b8
AbramQaBugHunter <AbramQa@ya.ru>
2022-12-15 11:02:08 +0200

    add test_get_user_details_auth_as_same_user in test_user_get.py and add def assert_json_has_keys in Assertions

--- Libary/assertions.py
+++ Libary/assertions.py
@@ -24,13 +24,23 @@ class Assertions:
         assert name in response_as_dict, f"Response JSON doesn`t have key'{name}'"
 
     @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not on JSON format. Response text is '{response.text}'"
+
+        for name in names:    
+            assert name in response_as_dict, f"Response JSON doesn`t have key'{name}'"
+
+    @staticmethod
     def assert_json_has_not_key(response: Response, name):
         try:
             response_as_dict = response.json()
         except json.JSONDecodeError:
             assert False, f"Response is not on JSON format. Response text is '{response.text}'"
 
-        assert name not in response_as_dict, f"Response JSON shouldn`t have key'{name}'"
+        assert name not in response_as_dict, f"Response JSON shouldn`t have key'{name}'. But it`s present"
 
     @staticmethod
     def assert_code_status(response: Response, expected_status_code):
